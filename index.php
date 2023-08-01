b2f2b318bd36e23fd64340fbd728fee5fca46bfd
vsnair85 <vishnusnair2008@gmail.com>
2023-08-01 22:21:06 +0530

    minor edit

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="www.vishnusnair.com">
     <title>Password Encryption</title>
 </head>
 
 <body>
     <h1>Enter Your Password to encrypt</h1>
     <form action="" method="post">
-    <input type="text" name="ppqs">
-    <button type="submit">Submit</button>
-    </form>    
+        <input type="text" name="ppqs">
+        <button type="submit">Submit</button>
+    </form>
     <?php
-    $ppqs = isset($_REQUEST['ppqs'])?$_REQUEST['ppqs']:"";
-    $pwd="";
-    if(!empty($ppqs)){
+    $ppqs = isset($_REQUEST['ppqs']) ? $_REQUEST['ppqs'] : "";
+    $pwd = "";
+    if (!empty($ppqs)) {
         $pwd = password_hash($ppqs, PASSWORD_DEFAULT);
-    }    
+    }
     ?>
     <div>
         <p><?php echo "<span style='font-weight: bold;'>Your Password: </span>" . $ppqs . "<br> <span style='font-weight: bold;'>Your Encrypted Password: </span>" . $pwd; ?></p>
